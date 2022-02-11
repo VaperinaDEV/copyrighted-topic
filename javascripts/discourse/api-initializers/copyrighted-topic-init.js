0ffd863c5354efd6e9bfd8e98c1807f1a42800c0
Don <71207900+VaperinaDEV@users.noreply.github.com>
2022-02-11 11:54:59 +0100

    Update copyrighted-topic-init.js

--- javascripts/discourse/api-initializers/copyrighted-topic-init.js
+++ javascripts/discourse/api-initializers/copyrighted-topic-init.js
@@ -6,17 +6,17 @@ export default {
     withPluginApi("0.8.7", (api) => {
       api.onPageChange((url, title) => {
         const copyrightTag = settings.copyright_tag;
-        const body = $("body.tag-" + copyrightTag + " .container.posts");
+        const copyrightBody = $("body.tag-" + copyrightTag + " .container.posts");
         const topicCheck = $(".archetype-regular");
         const disableContextmenu = settings.disable_contextmenu;
         const disableCopy = settings.disable_copy;
 
-        if (disableContextmenu && body && topicCheck) {
-          body.on('contextmenu', '#post_1 .cooked', function(e){ return false; });
+        if (disableContextmenu && copyrightBody && topicCheck) {
+          copyrightBody.on('contextmenu', '#post_1 .cooked', function(e){ return false; });
         }
         
-        if (disableCopy && body && topicCheck) {
-          body.bind('copy', '#post_1 .cooked', function(e){ return false; });
+        if (disableCopy && copyrightBody && topicCheck) {
+          copyrightBody.bind('copy', '#post_1 .cooked', function(e){ return false; });
         }
       });
     });
